4a6f775c4ea3a70c6a0d0b58a86f04b296bb0e4c
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-16 15:44:19 +1030

    uploading main App component

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+// import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
+// define component
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main></Main>
+      <Footer/>
+    </div>
+  );
+}
+
+// export component
+export default App;
