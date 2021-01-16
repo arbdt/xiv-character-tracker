cd7598b4eb6b9b2ea35a897543aafb1035a1b689
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-16 15:39:41 +1030

    uploading basic react files

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles/index.css"; // style sheet
+import App from './App';
+
+// render react index app etc
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
