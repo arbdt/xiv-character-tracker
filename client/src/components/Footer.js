d3d9976e47c83c745bbac51c5208f2905b8c762d
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-16 15:42:14 +1030

    uploading page footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+// define component
+function Footer(){
+    return (
+        <footer className="footer">
+            Made by <a href="https:github.com/arbdt">Arbdt</a> in 2021 using React and Mongoose. Powered by the XIVAPI. FFXIV (c) Square Enix.
+        </footer>
+    );
+}
+
+// export component
+export default Footer;
