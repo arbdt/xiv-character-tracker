b57c0348ac5cc8266103b1d5926a55ab3e955f5c
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-16 15:42:50 +1030

    uploading page header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+// define component
+function Header(){
+    return (
+        <header className="header">
+            <h1>XIV Character Tracker</h1>
+        </header>
+    );
+}
+
+// export component
+export default Header;
