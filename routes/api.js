bbad639d7cd86d24e5c546d1da21277380a9870a
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-18 20:23:46 +1030

    added GET and POST routes for character data

--- routes/api.js
+++ routes/api.js
@@ -23,15 +23,30 @@ router.get("/api/user/:userId", function(request, repsonse){
 // CHARACTER API -----
 
 // get character data
-router.get("/api/character/:charcterId", function( request, response){
-    characterController.read()
+router.get("/api/character/:characterId", function( request, response){
+    let idNum = parseInt(request.params.characterId);
+    Character.findOne({charId: idNum}).then( (result) => {
+        response.json(result);
+    });
 
 });
 
 // post new character data
-router.post("/api/character/:characterId", function (request, response){
+router.post("/api/character/", function (request, response){
     let newCharacter = {
-        
-    }
-    Character.create()
+        charId: request.body.charId,
+        charName: request.body.charName,
+        charServer: request.body.charServer,
+        charPortrait: request.body.charPortrait,
+        charClasses: request.body.charClasses,
+        minionCount: request.body.minionCount,
+        mountCount: request.body.mountCount
+    };
+
+    Character.create(newCharacter).then( result => {
+        response.json(result);
+    });
 });
+
+// export module
+module.exports = router;
