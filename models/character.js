7209472a677271b367777ba29a340b1f00051bea
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-18 17:19:30 +1030

    expanded on character model properties

--- models/character.js
+++ models/character.js
@@ -7,10 +7,20 @@ const classJobSchema = require("./classjob");
 
 // define User Schema
 const characterSchema = new Schema({
+    charId: {type: Number, require: true},
     charName: {type: String, required: true},
     charServer: {type: String, required: true},
-    charClasses: [classJobSchema]
+    charPortrait: {type: String},
+    charClasses: [classJobSchema],
+    achievementCount: {type: Number, default: 0},
+    achievementPoints: {type: Number, default: 0},
+    minionCount: {type: Number, default: 0},
+    mountCount: {type: Number, default: 0},
+    dateUpdated: {type: Date, default: Date.now()}
 });
 
+// define model
+const Character = mongoose.model("Character", characterSchema);
+
 // export
-module.exports = characterSchema;
+module.exports = {characterSchema, Character};
