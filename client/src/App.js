970865f313a7f41ad579c049df4b782c03a3e21f
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-18 17:37:22 +1030

    changed APP component content to Router Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 // import components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
+import {BrowserRouter, Route} from "react-router-dom";
+import CharacterSheet from "./components/CharacterSheet";
+import UserPage from "./components/UserPage";
+import CharSearch from "./components/CharSearch";
 
 // define component
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main></Main>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+        <main className="container">
+          <Route path="/character/:charId" component={CharacterSheet}></Route>
+          <Route path="/user" component={UserPage}></Route>
+          <Route path= "/search" component={CharSearch}></Route>
+        </main>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
