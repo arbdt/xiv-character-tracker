219c59f975e1256f1e11ff45e7e3cf449b9a3f07
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-18 17:21:12 +1030

    uploading user page component skeleton

---
+++ client/src/components/UserPage.js
@@ -0,0 +1,18 @@
+// this component is displayed to logged-in users
+// it will display a list of saved characters with options to delete or refresh each character, or click on a character for details
+
+// function to call database to retrieve user-associated content
+function getUserChars(userToken){
+
+}
+
+// define component
+function UserPage(props){
+    // display list of registered characters
+    // click through to dislay Character Sheet,
+    // or delete or update
+    
+}
+
+// export component
+export default UserPage;
