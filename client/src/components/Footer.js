b40a37c870f13ca2a1f60c2884a891bab489a0a5
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-20 14:29:53 +1030

    added links to technologies identified in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,8 @@
 function Footer(){
     return (
         <footer className="footer">
-            Made by <a href="https:github.com/arbdt">Arbdt</a> in 2021 using React and Mongoose. Powered by the XIVAPI. FFXIV (c) Square Enix.
+            Made by <a href="https:github.com/arbdt">Arbdt</a> in 2021 using <a href="https://reactjs.org/">React</a> and <a href="https://mongoosejs.com/">Mongoose</a>.
+             Powered by <a href="https://github.com/xivapi/xivapi-js">XIVAPI</a>. FFXIV &copy; 2010 Square Enix.
         </footer>
     );
 }
