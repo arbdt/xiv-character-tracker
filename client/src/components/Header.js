4db15f691a53da029b4eca3f0b609433936f1f6c
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-20 14:27:43 +1030

    corrected 2 instances of 'class' to 'className'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,15 +8,15 @@ function Header(){
         <header className="header">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">
-                    <img src={logo} width="30" height="30" class="d-inline-block align-top" alt="logo"/> XIV Character Tracker
+                    <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="logo"/> XIV Character Tracker
                 </a>
 
                 <ul className="navbar-nav">
                     <li className="nav-item active">
                         <Link className="nav-link" to="/search">Search</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link to="/user" className="nav-link" userId="1">User Page</Link>
+                    <li className="nav-item">
+                        <Link to="/user" className="nav-link" userId="">User Page</Link>
                     </li>
                 </ul>
             </nav>
