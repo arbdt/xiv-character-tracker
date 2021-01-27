dd23b5670801de8f0da134a44b8929f8943932f6
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-27 18:03:36 +1030

    moved Router and Auth0Provider components to Index from App

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,34 @@
 // import components
-import React, {useState} from "react";
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {BrowserRouter, Route} from "react-router-dom";
+import { Route} from "react-router-dom";
 import CharacterSheet from "./components/CharacterSheet";
 import UserPage from "./components/UserPage";
 import CharSearch from "./components/CharSearch";
 import Main from "./components/Main";
-import UserContext from "./UserContext";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // define component
 function App() {
 
-  // useState
-  let [loginState, setLoginState] = useState({user: {userId: ""}});
+  const {isLoading} = useAuth0();
 
-  //login and logout capabilities
-  function login(user){
-    setLoginState({user: user});
+  if (isLoading){
+    return (<div>Page is loading...</div>);
   }
 
-  function logout() {
-    setLoginState({user:{}});
-  }
-
-  // app rendering
+    // app rendering
   return (
     <div className="App">
-      <BrowserRouter>
-        {/*<Auth0Provider>*/}
-            <Header/>
-              <main className="container">
-                <Route path="/character/:charId" component={CharacterSheet}></Route>
-                <Route path="/user/:userId" component={UserPage}></Route>
-                <Route path="/search" component={CharSearch}></Route>
-                <Route exact path= "/" component={Main}></Route>
-              </main>
-            <Footer/>
-        {/*</Auth0Provider>*/}
-      </BrowserRouter>
+      <Header/>
+        <main className="container">
+          <Route path="/character/:charId" component={CharacterSheet}></Route>
+          <Route path="/user" component={UserPage}></Route>
+          <Route path="/search" component={CharSearch}></Route>
+          <Route exact path= "/" component={Main}></Route>
+        </main>
+      <Footer/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import "./styles/index.css"; // style sheet
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"; // for Auth0
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history"; // for Auth0
-import { Auth0Provider } from '@auth0/auth0-react';
 
 // render react index app etc
 ReactDOM.render(
