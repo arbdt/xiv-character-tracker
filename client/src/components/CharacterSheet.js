5e35cbc049f0335fb3c3c741bad765e73c2a4f9d
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-29 15:29:08 +1030

    restructured character sheet to move component-related functions inside component definition

--- client/src/components/CharacterSheet.js
+++ client/src/components/CharacterSheet.js
@@ -1,43 +1,14 @@
 // this component is for a page or section to display the retrieved and/or saved details for a specified character
 
-// imports
+// imports -----
 import React, {useEffect, useState} from "react";
 import ClassJob from "./ClassJob";
 import axios from "axios";
 import XIVAPI from "xivapi-js";
 const xiv = new XIVAPI();
 
-// function to retrieve data from XIVAPI
-const getXivapiData = async (char_id) => {
-    try{
-        let result = await xiv.character.get(char_id);
-        return result;
-    } catch(error) {
-        console.error(error);
-    }
-}
 
-// call API to get existing information from server
-const getMongooseData = async (character) => {
-    try {
-        let response = await axios.get(`/api/character/${character}`);
-        if (response.data !== null){
-            // return the data if it exists
-            console.log(response.data);
-            return response.data;
-        }
-    } catch (error){
-        console.error(error);
-    }
-}
-
-// function to save data
-function saveCharData(character){
-// post character data to db
-    axios.post(`/api/character`, character);
-}
-
-// define component
+// define component -----
 function CharacterSheet(props){
     // vars
     let characterId = props.match.params.charId; // get charId from URL
@@ -69,8 +40,6 @@ function CharacterSheet(props){
         minionCount: 0,
         mountCount: 0,
     });
-
-
 
     // useEffect to call XIVAPI for new information
     useEffect( () =>{
@@ -110,26 +79,57 @@ function CharacterSheet(props){
     
     },[characterId]);
 
-    // component output    
+    // additional functions -----
+    // function to retrieve data from XIVAPI
+    const getXivapiData = async (char_id) => {
+        try{
+            let result = await xiv.character.get(char_id);
+            return result;
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    // call API to get existing information from server
+    const getMongooseData = async (character) => {
+        try {
+            let response = await axios.get(`/api/character/${character}`);
+            if (response.data !== null){
+                // return the data if it exists
+                console.log(response.data);
+                return response.data;
+            }
+        } catch (error){
+            console.error(error);
+        }
+    }
+
+    // function to save data
+    const saveCharData = (character) => {
+    // post character data to db
+        axios.post(`/api/character`, character);
+    }
+
+
+    // component output -----
     return(
         <div className="card characterSheet">
             <h3 className="card-title">{freshCharacter.charName !== ""?  `${freshCharacter.charName} of ${freshCharacter.charServer}` : `Loading data...`} </h3>
             
             <div className="card-body">
-            <button onClick={() => saveCharData(freshCharacter)}><i className="fas fa-save"></i> Save</button>
+            <button className="btn btn-success" onClick={() => saveCharData(freshCharacter)}><i className="fas fa-save"></i> Click here to manually save data.</button>
                 <div className="row">
-                    <div className="col">
+                    <div className="col-4">
+                        <img src={freshCharacter.charAvatar} alt={freshCharacter.charName}/>
+                    </div>
+                    <div className="col-8">
                         <p>Minions: {freshCharacter.minionCount} {oldCharacter !== undefined && freshCharacter.minionCount > oldCharacter.minionCount ?
                             <span className="trackedChange"> +{freshCharacter.minionCount - oldCharacter.minionCount}</span> : <></>}</p>
                         <p>Mounts: {freshCharacter.mountCount} {oldCharacter !== undefined && freshCharacter.mountCount > oldCharacter.mountCount ?
                             <span className="trackedChange"> +{freshCharacter.mountCount - oldCharacter.mountCount}</span> : <></>}</p>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <img src={freshCharacter.charAvatar} alt={freshCharacter.charName}/>
-                    </div>
-                    <div className="col">
+                <div className="row row-cols-sm-2">
                         {freshCharacter.charClasses !== undefined? freshCharacter.charClasses.map( (classJob) => {
                             let oldJob = {};
                             if (oldCharacter !== undefined){
@@ -143,9 +143,7 @@ function CharacterSheet(props){
                                 <ClassJob newJobData={classJob} oldJobData={oldJob} key={classJob.classjobName}></ClassJob>
                             );
                         }) : <></>}
-                    </div>
-                </div>
-                
+                </div>  
             </div>
         </div>
     )
