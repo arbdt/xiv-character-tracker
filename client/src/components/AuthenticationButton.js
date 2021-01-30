1494229d7d42b6b3a9bcea46b2584aa8e2dc8911
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-30 13:59:26 +1030

    added functionality to check for/create local db user data on successful login

--- client/src/components/AuthenticationButton.js
+++ client/src/components/AuthenticationButton.js
@@ -7,11 +7,34 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
 import { useAuth0 } from "@auth0/auth0-react";
+import Axios from "axios";
 
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
+  const {user} = useAuth0();
 
-  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+  // create user DB entry on login if not exist
+  const makeUserData = async () => {
+      if (user){
+        try {
+          await Axios.post("/api/user", {userId: user.sub}).then (result => {
+            if (result.data !== null){
+              console.log("User is registered!");
+            }
+          });
+        } catch (error){
+          console.error(error);
+        }
+      }
+  }
+  // run make user data
+  if (isAuthenticated){
+    makeUserData();
+  }
+
+  return (
+    isAuthenticated? <LogoutButton /> : <LoginButton />
+  );
 };
 
 export default AuthenticationButton;
