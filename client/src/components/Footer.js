7c3f041be0d73c8dfa27e3d5f12b8e30315d5055
arbdt <67365149+arbdt@users.noreply.github.com>
2021-02-03 21:52:51 +1030

    made minor alteration to Square Enix copyright acknowledgement

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
     return (
         <footer className="footer">
             Made by <a href="https:github.com/arbdt">Arbdt</a> in 2021 using <a href="https://reactjs.org/">React</a> and <a href="https://mongoosejs.com/">Mongoose</a>.
-             Powered by <a href="https://github.com/xivapi/xivapi-js">XIVAPI</a>. FFXIV &copy; 2010 Square Enix.
+             Powered by <a href="https://github.com/xivapi/xivapi-js">XIVAPI</a>. FINAL FANTASY XIV &copy; SQUARE ENIX CO., LTD.
         </footer>
     );
 }
