49c95022cb704c6ebef786837ab1482a18b39c32
arbdt <67365149+arbdt@users.noreply.github.com>
2021-02-04 16:02:49 +1030

    lets try ading a build folder again even though it may or may not be necessary

---
+++ client/build/precache-manifest.4b533ce58265bb62c74d4b42bb0051d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3eb4bd92c4984218cf05a4f914e891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66c4f442ff9786e651f",
+    "url": "/static/css/main.bd4b7f04.chunk.css"
+  },
+  {
+    "revision": "5e5a4f284eca2f74f5da",
+    "url": "/static/js/2.6f15d3a8.chunk.js"
+  },
+  {
+    "revision": "fb9bd35292484e6192ae7ae0a07739b8",
+    "url": "/static/js/2.6f15d3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66c4f442ff9786e651f",
+    "url": "/static/js/main.167a1239.chunk.js"
+  },
+  {
+    "revision": "5e53f24f40ded94d8ac7",
+    "url": "/static/js/runtime-main.b4786107.js"
+  },
+  {
+    "revision": "2ea2ba20a02ecee13c03ba78b335f5e3",
+    "url": "/static/media/logo.2ea2ba20.png"
+  }
+]);
